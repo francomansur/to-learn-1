ef4704409fe52ce0b371d92618695358edab62bc
1161731 <1161731@liae.edu.uniube.br>
2026-03-10 21:32:02 -0300

    aiiiin

--- src/index.php
+++ src/index.php
@@ -1,5 +1,12 @@
-<?php
-
-for ($i = 1; $i <= 10; $i++) {
-    echo $i . "\n";
-}
+<html>
+    <head>
+        <title>SISTEMA</title>
+    </head>
+    <body>
+        <form action="processo.php" method="post">
+            CPF:<input type="text" name="CPF" placeholder="CPF"> <br>
+            SENHA:<input type="password" name="senha" placeholder="SENHA"><br>
+            <input type="submit" value="Entrar">
+        </form>
+    </body>
+</html>
---
+++ src/processo.php
@@ -0,0 +1,6 @@
+<?php
+
+$cpf = $_POST['CPF'];
+$senha = $_POST['senha'];
+
+echo $cpf . '-' . $senha;
